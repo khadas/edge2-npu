2b34bda7e74b329b2d547122cd4f3cf66a60a51f
Frank Wang <yan-wyb@foxmail.com>
2022-06-09 17:44:29 +0800

    C++: fix yolov5 demo error
    Signed-off-by: Frank Wang <yan-wyb@foxmail.com>

--- C++/yolov5/src/postprocess.cc
+++ C++/yolov5/src/postprocess.cc
@@ -23,7 +23,7 @@
 
 #include <set>
 #include <vector>
-#define LABEL_NALE_TXT_PATH "./model/coco_80_labels_list.txt"
+#define LABEL_NALE_TXT_PATH "./data/coco_80_labels_list.txt"
 
 static char* labels[OBJ_CLASS_NUM];
 
